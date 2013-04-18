bc5894b71229a187b99412a76fda914e8dbd8836
Sindre Sorhus <sindresorhus@gmail.com>
2013-04-19 00:50:18 +0200

    init

---
+++ test.js
@@ -0,0 +1,41 @@
+/*global describe, it */
+'use strict';
+var assert = require('assert');
+var dargs = require('./dargs');
+
+
+var fixture = {
+	a: 'foo',
+	b: true,
+	c: false,
+	d: 5,
+	e: ['foo', 'bar'],
+	f: null,
+	g: undefined,
+	camelCaseCamel: true
+};
+
+describe('dargs()', function () {
+	it('convert options to cli flags', function () {
+		var actual = dargs(fixture);
+		var expected = [
+			'--a', 'foo',
+			'--b',
+			'--d', '5',
+			'--e', 'foo',
+			'--e', 'bar',
+			'--camel-case-camel'
+		];
+		assert.deepEqual(actual, expected);
+	});
+
+	it('exclude options', function () {
+		var actual = dargs(fixture, ['b', 'e']);
+		var expected = [
+			'--a', 'foo',
+			'--d', '5',
+			'--camel-case-camel'
+		];
+		assert.deepEqual(actual, expected);
+	});
+});
